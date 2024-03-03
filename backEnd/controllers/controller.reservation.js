66e95829fa24ca27460bff7d2b2c7efa7322f693
yacine-bencheikh <ybenchikh472@gmail.com>
2024-03-03 18:18:06 +0100

    "done"

--- backEnd/controllers/controller.reservation.js
+++ backEnd/controllers/controller.reservation.js
@@ -1,21 +1,43 @@
-const {Reservation} = require("../models/models");
+const { Reservation } = require("../models/models");
 
-module.exports ={
-    makeReservation: async (req,res)=>{
+module.exports = {
+    makeReservation: async (req, res) => {
         try {
-            
+
         } catch (error) {
             console.log(error);
         }
     },
-    uploadData: async (req,res)=>{
+    uploadData: async (req, res) => {
         try {
-            console.log(req.body);
+            const lines = req.body.data.split('\n')
+            let result = []
+            for (var i = 0; i < lines.length; i++) {
+                var obj = {
+                    codeActivation: "",
+                    etat: -1,
+                    iccid: "",
+                    pin1: "",
+                    puc1: "",
+                    pin2: "",
+                    puc2: "",
+                    msisdn: ""
+                }
+                obj.codeActivation = lines[i][6]
+                obj.iccid = lines[i][1]
+                obj.msisdn = lines[i][0]
+                obj.pin1 = lines[i][2]
+                obj.puc1 = lines[i][3]
+                obj.pin2 = lines[i][4]
+                obj.puc2 = lines[i][5]
+                result.push(obj)
+            }
+            console.log(result);
             res.send('Data received');
         } catch (error) {
             console.log(error);
             res.status(500).send('Server error');
         }
     }
-    
+
 }
--- frontEnd/screens/UploadFileScreen.js
+++ frontEnd/screens/UploadFileScreen.js
@@ -1,6 +1,7 @@
 import { StyleSheet, Text, View, SafeAreaView, Button } from 'react-native';
 import React, { useState } from 'react';
 import * as DocumentPicker from 'expo-document-picker';
+import * as FileSystem from 'expo-file-system';
 
 import axios from 'axios';
 
@@ -12,12 +13,9 @@ const UploadFileScreen = () => {
             const result = await DocumentPicker.getDocumentAsync({
                 type: '*/*', // You can specify the MIME type here to filter the documents
             });
-            console.log();  
             if (!result.canceled) {
                 const fileContent = await FileSystem.readAsStringAsync(result.assets[0].uri, { encoding: FileSystem.EncodingType.UTF8 });
-                console.log(fileContent);
-                const response = await axios.post('http://10.0.2.2:3100/api/reservations/uploadData', fileContent);
-                console.log(response.data);
+                const response = await axios.post('http://10.0.2.2:3100/api/reservations/uploadData', { data: fileContent });
             }
         } catch (error) {
             console.log('Error picking document:', error);
@@ -27,7 +25,7 @@ const UploadFileScreen = () => {
         <SafeAreaView>
             <View>
                 <Text>UploadFileScreen</Text>
-                <Button title="Pick Document" onPress={()=>{pickDocument();console.log(selectedDocument)}} />
+                <Button title="Pick Document" onPress={() => { pickDocument(); console.log(selectedDocument) }} />
                 {selectedDocument && (
                     <Text>Selected Document: {selectedDocument.name}</Text>
                 )}
